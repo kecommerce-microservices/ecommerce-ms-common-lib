fe11040233651b687ba11bc3bd0af5a46756069d
Kauã Pereira <zkauacodes@hotmail.com>
2024-08-17 12:55:23 -0300

    feat(internal-error-exception): receive status code

--- domain/src/main/java/com/kaua/ecommerce/lib/domain/exceptions/InternalErrorException.java
+++ domain/src/main/java/com/kaua/ecommerce/lib/domain/exceptions/InternalErrorException.java
@@ -1,20 +1,36 @@
 package com.kaua.ecommerce.lib.domain.exceptions;
 
-public class InternalErrorException extends NoStacktraceException{
+public class InternalErrorException extends NoStacktraceException {
 
-    private InternalErrorException(final String message) {
+    private final int statusCode;
+
+    private InternalErrorException(final String message, int statusCode) {
         super(message);
+        this.statusCode = statusCode;
     }
 
-    private InternalErrorException(final String message, final Throwable cause) {
+    private InternalErrorException(final String message, final Throwable cause, int statusCode) {
         super(message, cause);
+        this.statusCode = statusCode;
     }
 
-    public static InternalErrorException with(final String message) {
-        return new InternalErrorException(message);
+    public static InternalErrorException with(final String message, int statusCode) {
+        return new InternalErrorException(message, statusCode);
+    }
+
+    public static InternalErrorException with(final String message, final int statusCode, final Throwable cause) {
+        return new InternalErrorException(message, cause, statusCode);
+    }
+
+    public static InternalErrorException with(final String message ) {
+        return new InternalErrorException(message, 0);
     }
 
     public static InternalErrorException with(final String message, final Throwable cause) {
-        return new InternalErrorException(message, cause);
+        return new InternalErrorException(message, cause, 0);
+    }
+
+    public int getStatusCode() {
+        return statusCode;
     }
 }
